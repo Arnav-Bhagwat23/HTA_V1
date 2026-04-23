4fe88fabd1d1cb76a85f575630f9f974fe954a53
Arnav Bhagwat <arnavbhagwat23@gmail.com>
2026-04-23 16:11:54 +0100

    Finalize shared package exports and config

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,16 @@
+export * from './constants/countries';
+export * from './constants/job-modes';
+export * from './constants/job-statuses';
+export * from './constants/source-types';
+export * from './constants/warning-codes';
+
+export * from './schemas/preview-response';
+export * from './schemas/search-request';
+export * from './schemas/upload-request';
+
+export * from './types/api';
+export * from './types/common';
+export * from './types/documents';
+export * from './types/extraction';
+export * from './types/job-status';
+export * from './types/normalized-query';
